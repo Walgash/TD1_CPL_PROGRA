b7e534f5c219d8c455f0d148b8cb6338868e9085
Rakotonirina Mano <mano.rakotonirina@ens.uvsq.fr>
2021-09-30 13:41:19 +0200

    Classses Fraction et Main fonctionnelles

---
+++ src/Fraction.java
@@ -0,0 +1,15 @@
+public class Fraction {
+    private int num;
+    private int denom;
+
+    public Fraction(int num,int denom){
+        this.num = num;
+        this.denom = denom;
+    }
+
+    public String toString(){
+        return "Numerateur = "+num+ " / Denominateur = "+denom ;
+    }
+
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args ) {
+        System.out.println("Hello World !");
+        Fraction f =  new Fraction(1,2);
+        String test = f.toString();
+        System.out.println(test);
+    }
+    //bonjour ceci est un commentaire écrit depuis la forge
+}
